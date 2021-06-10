ef5e6217e83aa96778daaf76e7b266b60f3c6b83
Julian <julian1986.producciones@gmail.com>
2021-06-10 00:04:42 -0300

    desafio1

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let nombre = prompt('Ingresa tu nombre')
+let apellido = prompt('Ingresa tu apellido')
+let edad = prompt('Ingresa tu edad')
+let numeroFavorito = prompt('Cual es tu numero favorito')
+
+alert(`Tu nombre es ${nombre} ${apellido} y tenes ${edad}. Tu numero favorito es ${numeroFavorito}`)
+
+console.log(`Tu nombre es ${nombre} ${apellido} y tenes ${edad}. Tu numero favorito es ${numeroFavorito}`)
+
+
+
+
